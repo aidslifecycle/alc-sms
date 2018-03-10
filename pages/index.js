4cfac94fc21e5d19b18a4d615273d3f3a54f48f4
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-03-10 00:10:11 -0800

    Link as ID

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,21 @@ import Link from 'next/link';
 const PostLink = (props) => {
   return (
     <li>
-      <Link href={`/post?title=${props.title}`}>
+      <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
         <a>{props.title}</a>
       </Link>
     </li>
   );
-}
+};
 
 const Index = () => {
 	return (<Layout>
            <ul>
-             <PostLink title="Hello Next.js" />
-             <PostLink title="Learn Next.js" />
-             <PostLink title="Depoly or something" />
+             <PostLink id="hello-nextjs" title="Hello Next.js" />
+             <PostLink id="learn-nextjs" title="Learn Next.js" />
+             <PostLink id="deploy-nextjs" title="Deploy or something" />
            </ul>
          </Layout>);
-}
+};
 
 export default Index;
