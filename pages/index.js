6371864ee0240fee424ae446070780bae0a06cbe
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-03-13 11:00:31 -0700

    Format display for group pages

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Layout from '../components/Layout';
 import GroupsSidebar from '../components/GroupsSidebar';
 import ChannelFeed from '../components/ChannelFeed';
-import Link from 'next/link';
 
 const Index = () => {
 	return (<Layout>
            <div className="container">
-			 <div className="row">
-				 <GroupsSidebar />
-				 <ChannelFeed />
+             <div className="row">
+               <GroupsSidebar />
+               <ChannelFeed />
              </div>
            </div>
          </Layout>);
--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,17 @@
 import Layout from '../components/Layout';
 
 const Post = (props) => {
-	console.log("PROPS.URL", props.url);
-	return (<Layout>
-           <h1>{ props.url.query.title }</h1>
-           <p>This is the blog content</p>
-         </Layout>)
+
+  return (<Layout>
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-12">
+                  <h1>{ props.url.query.title }</h1>
+                  <p>This is the { props.url.query.title } page</p>
+                </div>
+              </div>
+            </div>
+          </Layout>);
 };
 
 export default Post;
