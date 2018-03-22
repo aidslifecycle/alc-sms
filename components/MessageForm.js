a38f4cb131fea41e8851986a0cce4f4f14353330
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-03-21 20:32:37 -0700

    [wip] Basic method for MessageForm component

--- components/MessageForm.js
+++ components/MessageForm.js
@@ -1,17 +1,24 @@
-const MessageCard = (props) => {
-  return (
-    <div className="card">
-      <h5 className="card-header bg-primary text-white">{ props.title }</h5>
+import React from 'react';
+
+class MessageForm extends React.Component {
+
+  alertOnClick(e) {
+    e.preventDefault();
+  }
+
+  render () {
+    return (<div className="card">
+      <h5 className="card-header bg-primary text-white">{ this.props.title }</h5>
       <div className="card-body">
         <form>
-          <div class="form-group">
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter a message"></textarea>
+          <div className="form-group">
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter a message"></textarea>
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary" onClick={(e)=>this.alertOnClick(e)}>Submit</button>
         </form>
       </div>
-    </div>
-    );
+    </div>)
+  }
 };
 
-export default MessageCard;
+export default MessageForm;
