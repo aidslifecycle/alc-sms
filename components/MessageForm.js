68962c9818c1130ebaaaf43e0f11a64935c333db
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-03-28 23:18:50 -0700

    Client side Fetch

--- components/MessageForm.js
+++ components/MessageForm.js
@@ -3,17 +3,24 @@ import React from 'react';
 class MessageForm extends React.Component {
 
   sendMessage(event) {
-    //event.preventDefault();
-    
+    event.preventDefault();
+
+    fetch('sms/test', {
+      method: 'POST',body: {'message': 'heelo'},headers: {'content-type': 'application/json'}
+    }).then(res => {
+      return res.json();
+    }).then(firebase => {
+      console.log(firebase);
+    });
   }
 
   render() {
     return (<div className="card">
               <h5 className="card-header bg-primary text-white">{ this.props.title }</h5>
               <div className="card-body">
-                <form action="sms/test" method="post" onClick={ (e) => {
-                                                                  this.sendMessage(e);
-                                                                } }>
+                <form action="/" method="post" onClick={ (event) => {
+                                                           this.sendMessage(event);
+                                                         } }>
                   <div className="form-group">
                     <textarea className="form-control" name="message" id="message" rows="3" placeholder="Enter a message"></textarea>
                   </div>
